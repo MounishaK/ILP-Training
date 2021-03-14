747fd64d18233bc574f6c1f12434df18bc889b49
MounishaK <mounisha098@gmail.com>
2021-03-14 14:10:52 -0500

    Web Technologies- HTML,CSS

---
+++ WebTechnologies/JavaScript Programs/DOMoperation.js
@@ -0,0 +1,56 @@
+function displayDomMessage()
+{
+    alert("Welcome to external JS file");
+}
+//var empObj = [];
+/*function storeInSession()
+{
+    sessionStrogage.setItem("empInfo",empObj)
+}
+
+function retrive()
+{
+    
+}
+*/
+function onFormSubmit()
+{
+    alert("Event gernerated...")
+    //var data = readFormData();
+    //insertNewRecord(data);
+    //resetData();
+    ///empObj.push(data); // in empobj
+
+}
+
+function readFormData()
+{
+    var obj = {} // empty object
+    obj.name = document.getElementsById("user").value;
+    obj.name = document.getElementsById("age").value;
+    console.log(obj);
+    //return obj;
+}
+
+/*
+function insertNewRecord(data)
+{
+    var table = document.getElementById("employeeList")
+    var body = table.getElementByTagName("tbody")[0];
+    var newRow = body.insertRow(body.lenth); //row created
+
+    var cell1 = newRow.insertCell(0); //cell created
+    cell1.innerHTML = data.name; // value passed
+
+    var cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.age;
+}
+
+
+function resetData()
+{
+    document.getElementsById("name").value ="";
+    document.getElementsById("age").value = "";
+        
+}
+*/
---
+++ WebTechnologies/JavaScript Programs/jsondata.js
@@ -0,0 +1,35 @@
+/*
+object : any real world entity
+examples - person, car, animsl, bank,etc..
+*/
+
+// one way object creation using object literal style.
+
+var empobj = {id : 100, name: "Blue", salary: 12000};
+document.write("<br/>")
+document.write("Employee Object <br/>")
+document.write(empobj)
+document.write("<br/>")
+document.write("Id is " + empobj.id+"<br/>")
+document.write("Name is " + empobj.name+"<br/>")
+document.write("Salary is "+ empobj.salary+"<br/>")
+
+//convert onject into string Format
+var empString = JSON.stringify(empobj);
+document.write("<br/>")
+document.write("Employee Object in string fromat <br/>")
+document.write(empString)
+document.write("<br/>")
+document.write("Id is " + empString.id+"<br/>")
+document.write("Name is " + empString.name+"<br/>")
+document.write("Salary is "+ empString.salary+"<br/>")
+
+//convert string to JSON format
+var empJSON = JSON.parse(empString);
+document.write("<br/>")
+document.write("Employee Object in JSON fromat <br/>")
+document.write(empJSON)
+document.write("<br/>")
+document.write("Id is " + empJSON.id+"<br/>")
+document.write("Name is " + empJSON.name+"<br/>")
+document.write("Salary is "+ empJSON.salary+"<br/>")
